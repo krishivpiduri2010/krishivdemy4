ccd7014603569bdf0a4ae233d4fde5e9a1afee01
krishivpiduri2010 <59217701+krishivpiduri2010@users.noreply.github.com>
2021-08-10 16:35:50 -0700

    need to add dynamics

--- krishivdemy_app/models.py
+++ krishivdemy_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class Year(models.Model):
+    year = models.IntegerField()
+
+
+class Month(models.Model):
+    month = models.CharField(max_length=11)
+
+
+class Date(models.Model):
+    date = models.IntegerField()
