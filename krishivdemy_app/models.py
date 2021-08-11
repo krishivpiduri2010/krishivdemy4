d3e2007a5bdc60d59c7663104cd8d6cd1bce56c2
krishivpiduri2010 <59217701+krishivpiduri2010@users.noreply.github.com>
2021-08-10 19:31:01 -0700

    Dropdowns done, deed to add pages

--- krishivdemy_app/models.py
+++ krishivdemy_app/models.py
@@ -7,8 +7,10 @@ class Year(models.Model):
 
 
 class Month(models.Model):
+    year=models.ForeignKey(Year,on_delete=models.CASCADE,null=True)
     month = models.CharField(max_length=11)
 
 
 class Date(models.Model):
+    month=models.ForeignKey(Month,on_delete=models.CASCADE,null=True)
     date = models.IntegerField()
--- krishivdemy_app/views.py
+++ krishivdemy_app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import *
+
 
 # Create your views here.
 def home(request):
-    return render(request,'home.html')
+    return render(request, 'home.html',{'years': Year.objects.all(), 'months': Month.objects.all()
+        , 'days': Date.objects.all()})
